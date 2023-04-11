1ba8aa87a442114c1684bc55e7c8c7b74a17cfa5
NeiryAeris <122264722+NeiryAeris@users.noreply.github.com>
2023-04-11 13:57:26 +0700

    Add files via upload

--- AL11B/runner.cpp
+++ AL11B/runner.cpp
@@ -11,6 +11,7 @@ int main()
     cin >> number_of_points;
     for (int i = 0; i < number_of_points; i++)
     {
+        input.tag_number = i;   //Name the points with numbers
         cin >> input.x >> input.y >> input.z;
         points.push_back(input);
     }
@@ -23,8 +24,9 @@ int main()
 
     // Project the points from R^3 to R^2
     vector<Point2D> XYprojected = projectPoints(points, systemXY);
-    //Fetch first point(could say this is yi*)
+    //Fetch first point
     Point2D starting_point = find_lowest_y_highest_x(XYprojected);
+    Point2D starry_y = find_y_star_star(starting_point,XYprojected);
     // Print the projected points
     for (const auto &point : XYprojected)
     {
